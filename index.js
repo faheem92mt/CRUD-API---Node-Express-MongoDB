9a33c23fe0f7c59a3682ae90a2b73dcee421b4e6
faheem92mt <faheemibnhabib@gmail.com>
2024-03-27 01:43:09 +0800

    comment

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const app = express();
+
+const Product = require('./models/product.model.js');
+
 const mongoose = require('mongoose');
+
+
+app.use(express.json());
 
 app.get('/', (req,res)=> {
     res.send('hey');
 })
+
+app.post('/api/products', (req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
 
 app.listen(3000, ()=> {
     console.log('server is running on port 3000');
