f327a78a0e0a7dfdd80869bedcd21d165980ac7e
faheem92mt <faheemibnhabib@gmail.com>
2024-03-26 23:34:01 +0800

    comment

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.get('/', (req,res)=> {
-    res.send('hey')
+    res.send('hey');
 })
 
 app.listen(3000, ()=> {
     console.log('server is running on port 3000');
 });
+
+mongoose.connect("mongodb+srv://faheemibnhabib:whoneedsthis@shawarma.3oipfzz.mongodb.net/MyShawarmaNode-API?retryWrites=true&w=majority&appName=Shawarma")
+.then(()=> {
+    console.log("connected to db!");
+})
+.catch(() => {
+    console.log("connection failed!");
+})
