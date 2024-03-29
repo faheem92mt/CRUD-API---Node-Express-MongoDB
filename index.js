68cf9f832aa9e1513476baf60239ae490c15ca3b
faheem92mt <faheemibnhabib@gmail.com>
2024-03-30 02:01:27 +0800

    comment

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const Product = require('./models/product.model.js');
 
 const mongoose = require('mongoose');
 
-// middleware
+// we are not allowed to pass JSON through our node js by default
+// we have to use middleware - we have to configure it
 app.use(express.json());
 
 
@@ -13,15 +14,25 @@ app.get('/', (req,res)=> {
     res.send('heyo');
 })
 
-app.post('/api/products', (req, res) => {
+app.post('/api/products', async (req, res) => {
     // console.log(req.body);
     // res.send(req.body);
     try {
-
+        const product = await Product.create(req.body);
+        res.status(200).json(product); 
     } catch (error) {
         res.status(500).json({message: error.message});
     }
 });
+
+app.get('/api/products', async (req, res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
 
 app.listen(3000, ()=> {
     console.log('server is running on port 3000');
